c7c4473ee63a6c73cbe29c97b10cb5bca0833ebb
micdol <michael.dolbear@nasdaq.com>
2018-11-08 08:38:09 -0700

    Non-working examples of creating pods via api....believe issues with local env

---
+++ src/main/java/com/nasdaq/Example.java
@@ -0,0 +1,24 @@
+package com.nasdaq;
+
+import io.kubernetes.client.ApiClient;
+import io.kubernetes.client.ApiException;
+import io.kubernetes.client.Configuration;
+import io.kubernetes.client.apis.CoreV1Api;
+import io.kubernetes.client.models.V1Pod;
+import io.kubernetes.client.models.V1PodList;
+import io.kubernetes.client.util.Config;
+
+import java.io.IOException;
+
+public class Example {
+    public static void main(String[] args) throws IOException, ApiException{
+        ApiClient client = Config.defaultClient();
+        Configuration.setDefaultApiClient(client);
+
+        CoreV1Api api = new CoreV1Api();
+        V1PodList list = api.listPodForAllNamespaces(null, null, null, null, null, null, null, null, null);
+        for (V1Pod item : list.getItems()) {
+            System.out.println(item.getMetadata().getName());
+        }
+    }
+}
