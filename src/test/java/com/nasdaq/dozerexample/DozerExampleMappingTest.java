c9f8f9a6bcd4d59f0ff1c158fb299d8e75277708
micdol <michael.dolbear@nasdaq.com>
2018-11-12 11:09:39 -0700

    Updated test to verify values after transformation.

--- src/test/java/com/nasdaq/dozerexample/DozerExampleMappingTest.java
+++ src/test/java/com/nasdaq/dozerexample/DozerExampleMappingTest.java
@@ -20,16 +20,17 @@ public class DozerExampleMappingTest {
 
 
     /**
-     * Option mapping test
+     * Data mapping test
      */
     @Test
-    public void optionMappingTest() {
+    public void dataMappingTest() {
 
-        Mapper tempMapper;
-        Option tempOption;
-        Equity tempEquity;
-        Future tempFuture;
-        Instrument tempInstrument;
+        Mapper                  tempMapper;
+        Option                  tempOption;
+        Equity                  tempEquity;
+        Future                  tempFuture;
+        Instrument              tempInstrument;
+        Map<String, String>     tempMap;
 
         //Create the dozer mapper from the builder -- load the xml mapping files
         tempMapper = DozerBeanMapperBuilder
@@ -38,26 +39,35 @@ public class DozerExampleMappingTest {
                         .build();
 
         //Create an option from a hashmap
-        tempOption = tempMapper.map(this.createOptionMap(), Option.class);
-        assertTrue("Option is null", tempOption != null);
-
+        tempMap = this.createOptionMap();
+        tempOption = tempMapper.map(tempMap, Option.class);
+        assertTrue("Option is null", (tempOption != null)
+                                                && this.equals(tempMap,tempOption));
         //Create an equity from a hashmap
-        tempEquity = tempMapper.map(this.createEquityMap(), Equity.class);
-        assertTrue("Equity is null", tempEquity != null);
+        tempMap = this.createEquityMap();
+        tempEquity = tempMapper.map(tempMap, Equity.class);
+        assertTrue("Equity is null", tempEquity != null
+                                                            && this.equals(tempMap,tempEquity));
 
         //Create a future from a hashmap
-        tempFuture = tempMapper.map(this.createFutureMap(), Future.class);
-        assertTrue("Future is null", tempFuture != null);
+        tempMap = this.createFutureMap();
+        tempFuture = tempMapper.map(tempMap, Future.class);
+        assertTrue("Future is null", tempFuture != null
+                                                            && this.equals(tempMap,tempFuture));
 
         //Create an Instrument from a from a hashmap, but see if it creates the right subclass
-        tempInstrument = tempMapper.map(this.createFutureMap(), Instrument.class);
+        tempMap = this.createFutureMap();
+        tempInstrument = tempMapper.map(tempMap, Instrument.class);
         assertTrue("Instrument is null", tempInstrument != null
-                                                            && tempInstrument.getClass().equals(Future.class));
+                                                            && tempInstrument.getClass().equals(Future.class)
+                                                                && this.equals(tempMap,(Future)tempInstrument));
 
         //Create an Instrument from a hashnmap, but see if it creates the right subclass
-        tempInstrument = tempMapper.map(this.createOptionMap(), Instrument.class);
+        tempMap = this.createOptionMap();
+        tempInstrument = tempMapper.map(tempMap, Instrument.class);
         assertTrue("Option is null", tempInstrument != null
-                                                    && tempInstrument.getClass().equals(Option.class));
+                                                        && tempInstrument.getClass().equals(Option.class)
+                                                            && this.equals(tempMap,(Option)tempInstrument));
 
 
     }
@@ -108,24 +118,131 @@ public class DozerExampleMappingTest {
     }
 
     /**
+     * Answer whether or not anOption equals the values in aMap
+     * @param aMap Map
+     * @param anOption Option
+     */
+    private boolean equals(Map<String, String> aMap, Option anOption) {
+
+        boolean tempValue;
+
+        tempValue =
+            aMap.get(FinancialObjectBuilder.INSTRUMENT_TYPE_KEY).equals(anOption.getInstrumentType().name())
+                &&
+            aMap.get(FinancialObjectBuilder.EXTERNAL_ID_KEY).equals(anOption.getExternalId())
+                &&
+            aMap.get(FinancialObjectBuilder.ID_KEY).equals(anOption.getId().toString())
+                &&
+            aMap.get(FinancialObjectBuilder.SYMBOL_KEY).equals(anOption.getSymbol())
+                &&
+            aMap.get(FinancialObjectBuilder.UNDERLYING_ID_KEY).equals(anOption.getUnderlyingId().toString())
+                &&
+            LocalDate.parse(aMap.get(FinancialObjectBuilder.EXPIRATION_DATE_KEY)).equals(anOption.getExpirationDate())
+                &&
+            LocalDate.parse(aMap.get(FinancialObjectBuilder.EFFECTIVE_DATE_KEY)).equals(anOption.getEffectiveDate())
+                &&
+            aMap.get(FinancialObjectBuilder.CONTRACT_SIZE_KEY).equals(anOption.getContractSize().toString())
+                &&
+            aMap.get(FinancialObjectBuilder.OPTION_TYPE_KEY).equals(anOption.getOptionType().name())
+                &&
+            aMap.get(FinancialObjectBuilder.OPTION_STYLE_KEY).equals(anOption.getOptionStyle().name())
+                &&
+            aMap.get(FinancialObjectBuilder.STRIKE_PRICE_VALUE_KEY).equals(anOption.getStrikePrice().getValue().toString())
+                &&
+            aMap.get(FinancialObjectBuilder.STRIKE_PRICE_CURRENCY_KEY).equals(anOption.getStrikePrice().getCurrency())
+                &&
+            aMap.get(FinancialObjectBuilder.STRIKE_PRICE_TYPE_KEY).equals(anOption.getStrikePrice().getType().name())
+                &&
+            aMap.get(FinancialObjectBuilder.TIME_EROSION_KEY).equals(anOption.getTimeErosion().toString());
+
+        return tempValue;
+
+    }
+
+    /**
+     * Answer whether or not anEquity equals the values in aMap
+     * @param aMap Map
+     * @param anEquity Equity
+     */
+    private boolean equals(Map<String, String> aMap, Equity anEquity) {
+
+        boolean tempValue;
+
+        tempValue =
+                aMap.get(FinancialObjectBuilder.INSTRUMENT_TYPE_KEY).equals(anEquity.getInstrumentType().name())
+                        &&
+                        aMap.get(FinancialObjectBuilder.EXTERNAL_ID_KEY).equals(anEquity.getExternalId())
+                        &&
+                        aMap.get(FinancialObjectBuilder.ID_KEY).equals(anEquity.getId().toString())
+                        &&
+                        aMap.get(FinancialObjectBuilder.SYMBOL_KEY).equals(anEquity.getSymbol());
+
+
+
+
+        return tempValue;
+
+    }
+
+    /**
+     * Answer whether or not aFuture equals the values in aMap
+     * @param aMap Map
+     * @param aFuture Future
+     */
+    private boolean equals(Map<String, String> aMap, Future aFuture) {
+
+        boolean tempValue;
+
+        tempValue =
+                aMap.get(FinancialObjectBuilder.INSTRUMENT_TYPE_KEY).equals(aFuture.getInstrumentType().name())
+                        &&
+                        aMap.get(FinancialObjectBuilder.EXTERNAL_ID_KEY).equals(aFuture.getExternalId())
+                        &&
+                        aMap.get(FinancialObjectBuilder.ID_KEY).equals(aFuture.getId().toString())
+                        &&
+                        aMap.get(FinancialObjectBuilder.SYMBOL_KEY).equals(aFuture.getSymbol())
+                        &&
+                        aMap.get(FinancialObjectBuilder.UNDERLYING_ID_KEY).equals(aFuture.getUnderlyingId().toString())
+                        &&
+                        LocalDate.parse(aMap.get(FinancialObjectBuilder.EXPIRATION_DATE_KEY)).equals(aFuture.getExpirationDate())
+                        &&
+                        LocalDate.parse(aMap.get(FinancialObjectBuilder.EFFECTIVE_DATE_KEY)).equals(aFuture.getEffectiveDate())
+                        &&
+                        aMap.get(FinancialObjectBuilder.CONTRACT_SIZE_KEY).equals(aFuture.getContractSize().toString())
+                        &&
+                        aMap.get(FinancialObjectBuilder.FUTURE_TYPE_KEY).equals(aFuture.getFutureType().name())
+                        &&
+                        LocalDate.parse(aMap.get(FinancialObjectBuilder.FIRST_NOTICE_DATE_KEY)).equals(aFuture.getFirstNoticeDate())
+                        &&
+                        LocalDate.parse(aMap.get(FinancialObjectBuilder.LAST_TRADING_DATE_KEY)).equals(aFuture.getLastTradingDate());
+
+
+        return tempValue;
+
+    }
+
+
+
+
+    /**
      * Create equity map values
      */
     private Map<String, String> createEquityMap() {
 
-        Map<String, String> tempOptionMap = new HashMap<>();
+        Map<String, String> tempEquityMap = new HashMap<>();
 
-        tempOptionMap.put(FinancialObjectBuilder.INSTRUMENT_TYPE_KEY, "EQUITY");
-        tempOptionMap.put(FinancialObjectBuilder.EXTERNAL_ID_KEY, "2");
-        tempOptionMap.put(FinancialObjectBuilder.ID_KEY, UUID.randomUUID().toString());
-        tempOptionMap.put(FinancialObjectBuilder.SYMBOL_KEY, "IBM");
+        tempEquityMap.put(FinancialObjectBuilder.INSTRUMENT_TYPE_KEY, "EQUITY");
+        tempEquityMap.put(FinancialObjectBuilder.EXTERNAL_ID_KEY, "2");
+        tempEquityMap.put(FinancialObjectBuilder.ID_KEY, UUID.randomUUID().toString());
+        tempEquityMap.put(FinancialObjectBuilder.SYMBOL_KEY, "IBM");
 
-        tempOptionMap.put(FinancialObjectBuilder.UNDERLYING_ID_KEY, UUID.randomUUID().toString());
-        tempOptionMap.put(FinancialObjectBuilder.EXPIRATION_DATE_KEY, LocalDate.now().toString());
-        tempOptionMap.put(FinancialObjectBuilder.EFFECTIVE_DATE_KEY, LocalDate.now().toString());
-        tempOptionMap.put(FinancialObjectBuilder.CONTRACT_SIZE_KEY, "100000");
+        tempEquityMap.put(FinancialObjectBuilder.UNDERLYING_ID_KEY, UUID.randomUUID().toString());
+        tempEquityMap.put(FinancialObjectBuilder.EXPIRATION_DATE_KEY, LocalDate.now().toString());
+        tempEquityMap.put(FinancialObjectBuilder.EFFECTIVE_DATE_KEY, LocalDate.now().toString());
+        tempEquityMap.put(FinancialObjectBuilder.CONTRACT_SIZE_KEY, "100000");
 
 
-        return tempOptionMap;
+        return tempEquityMap;
     }
 
     /**
@@ -133,24 +250,24 @@ public class DozerExampleMappingTest {
      */
     private Map<String, String> createFutureMap() {
 
-        Map<String, String> tempOptionMap = new HashMap<>();
+        Map<String, String> tempFutureMap = new HashMap<>();
 
-        tempOptionMap.put(FinancialObjectBuilder.INSTRUMENT_TYPE_KEY, "FUTURE");
-        tempOptionMap.put(FinancialObjectBuilder.EXTERNAL_ID_KEY, "3");
-        tempOptionMap.put(FinancialObjectBuilder.ID_KEY, UUID.randomUUID().toString());
-        tempOptionMap.put(FinancialObjectBuilder.SYMBOL_KEY, "TXN");
+        tempFutureMap.put(FinancialObjectBuilder.INSTRUMENT_TYPE_KEY, "FUTURE");
+        tempFutureMap.put(FinancialObjectBuilder.EXTERNAL_ID_KEY, "3");
+        tempFutureMap.put(FinancialObjectBuilder.ID_KEY, UUID.randomUUID().toString());
+        tempFutureMap.put(FinancialObjectBuilder.SYMBOL_KEY, "TXN");
 
-        tempOptionMap.put(FinancialObjectBuilder.UNDERLYING_ID_KEY, UUID.randomUUID().toString());
-        tempOptionMap.put(FinancialObjectBuilder.EXPIRATION_DATE_KEY, LocalDate.now().toString());
-        tempOptionMap.put(FinancialObjectBuilder.EFFECTIVE_DATE_KEY, LocalDate.now().toString());
-        tempOptionMap.put(FinancialObjectBuilder.CONTRACT_SIZE_KEY, "100000");
+        tempFutureMap.put(FinancialObjectBuilder.UNDERLYING_ID_KEY, UUID.randomUUID().toString());
+        tempFutureMap.put(FinancialObjectBuilder.EXPIRATION_DATE_KEY, LocalDate.now().toString());
+        tempFutureMap.put(FinancialObjectBuilder.EFFECTIVE_DATE_KEY, LocalDate.now().toString());
+        tempFutureMap.put(FinancialObjectBuilder.CONTRACT_SIZE_KEY, "100000");
 
-        tempOptionMap.put(FinancialObjectBuilder.FUTURE_TYPE_KEY, "CURRENCY");
-        tempOptionMap.put(FinancialObjectBuilder.FIRST_NOTICE_DATE_KEY, LocalDate.now().toString());
-        tempOptionMap.put(FinancialObjectBuilder. LAST_TRADING_DATE_KEY, LocalDate.now().toString());
+        tempFutureMap.put(FinancialObjectBuilder.FUTURE_TYPE_KEY, "CURRENCY");
+        tempFutureMap.put(FinancialObjectBuilder.FIRST_NOTICE_DATE_KEY, LocalDate.now().toString());
+        tempFutureMap.put(FinancialObjectBuilder.LAST_TRADING_DATE_KEY, LocalDate.now().toString());
 
 
-        return tempOptionMap;
+        return tempFutureMap;
     }
 
 }
